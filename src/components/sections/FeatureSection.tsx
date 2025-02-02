f54aedb37429090e18e1c379e477ab26006d4d8b
Oladele Joshua O.D. <joshuaxp9@gmail.com>
2025-02-02 10:26:03 +0100

    Update FeatureSection.tsx

--- src/components/sections/FeatureSection.tsx
+++ src/components/sections/FeatureSection.tsx
@@ -4,21 +4,21 @@ function FeatureSection() {
   const data = [
     {
       icon: "/images/star_icon.svg",
-      title: "Budgeting Intervals",
+      title: "Easy & Fast Loan Access",
       description:
-        "Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.",
+        "Access quick and flexible loan options with a seamless application process. No long paperwork, no unnecessary delays—just instant financial support when you need it.",
     },
     {
       icon: "/images/mesh_icon.svg",
-      title: "Budgeting Intervals",
+      title: "Profitable Investment Opportunities",
       description:
-        "Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.",
+        "Grow your wealth with carefully curated investment plans designed for maximum returns. Whether you're a beginner or an experienced investor, our platform provides smart, risk-managed options to help you achieve your financial goals.",
     },
     {
       icon: "/images/cube_icon.svg",
-      title: "Budgeting Intervals",
+      title: "Secure & Transparent Transactions",
       description:
-        "Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.",
+        "Enjoy a safe, user-friendly platform with clear terms and no hidden fees. We provide clear terms, no hidden fees, and real-time tracking to ensure you stay in control of your finances.",
     },
   ];
   return (
@@ -32,7 +32,7 @@ function FeatureSection() {
             Features
           </p>
           <p className="text-black text-[48px] font-bold leading-tight">
-            Uifry Premium
+            With Asusu you can: 
           </p>
         </div>
         <div className="flex flex-col gap-[32px] mt-[32px]">
